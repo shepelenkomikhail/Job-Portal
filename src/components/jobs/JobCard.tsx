96c388f7ece69c99378ea3665aede5fc35813bdd
Mykhailo Shepelenko <mikhael2005@gmail.com>
2024-11-15 08:35:51 +0100

    grid card view

--- src/components/jobs/JobCard.tsx
+++ src/components/jobs/JobCard.tsx
@@ -23,7 +23,7 @@ export default function JobCard({vacancy, grid}: {vacancy: VacancyInterface, gri
             className={"border border-gray-400 rounded-xl p-4 transition-ease-in duration-300 hover:shadow-lg relative"}
             style={{width: grid ? "300px" : "100%"}}>
 
-            <button className="w-auto h-auto flex justify-center items-center rounded-full absolute top-0 right-0 -translate-x-1/4 translate-y-1/3">
+            <button className={`w-auto h-auto flex justify-center items-center rounded-full absolute top-0 right-0 ${!grid ? "-translate-x-1/4 translate-y-1/3": "translate-x-1/3 -translate-y-1/3"}`}>
                 <div onClick={() => {saveVacancy(vacancy);}} className="flex justify-center items-center">
                     <svg xmlns="http://www.w3.org/2000/svg" fill={saved ? "#F9E796" : "#ffffff"} width="40" height="40" viewBox="-5 -2 24 24" stroke="#000000">
                         <path d="M3 0h8a3 3 0 0 1 3 3v15a2 2 0 0 1-3.348 1.477L7.674 16.76a1 1 0 0 0-1.348 0l-2.978 2.717A2 2 0 0 1 0 18V3a3 3 0 0 1 3-3z"/>
@@ -33,25 +33,27 @@ export default function JobCard({vacancy, grid}: {vacancy: VacancyInterface, gri
 
             <div>
                 {grid ? (
-                    <div className={"flex items-center text-center"}>
-                        <div className={"flex flex-col"}>
-                            <div className={"flex flex-row gap-2"}>
-                                <img src={vacancy.companyLogo} className={"w-24 h-24 rounded-xl mb-2"}></img>
-                                <div className={"flex flex-col items-center"}>
+                    <div className={"flex mt-2"}>
+                        <div className={"flex flex-col items-center justify-center text-left"}>
+                            <div className={"flex flex-row gap-4"}>
+                                <img src={vacancy.companyLogo} className={"w-20 h-20 rounded-xl mb-2"}></img>
+                                <div className={"flex flex-col"}>
                                     <h2 className={"font-semibold"}>{vacancy.title}</h2>
                                     <p> {vacancy.company}</p>
-                                    <div className={"flex mt-2"}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#ef4444">
-                                            <path d="M480.06-486.67q30.27 0 51.77-21.56 21.5-21.55 21.5-51.83 0-30.27-21.56-51.77-21.55-21.5-51.83-21.5-30.27 0-51.77 21.56-21.5 21.55-21.5 51.83 0 30.27 21.56 51.77 21.55 21.5 51.83 21.5ZM480-168q129.33-118 191.33-214.17 62-96.16 62-169.83 0-114.86-73.36-188.1-73.36-73.23-179.97-73.23T300.03-740.1q-73.36 73.24-73.36 188.1 0 73.67 63 169.83Q352.67-286 480-168Zm0 88Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/>
-                                        </svg>
-                                        <p className={"font-medium"}>{vacancy.location}</p>
-                                    </div>
                                 </div>
                             </div>
-                            <button
-                                className={"mt-4 p-1.5 bg-red-100 border border-red-700 rounded-md hover:bg-red-700 hover:text-white transition-ease-in duration-300 text-red-900 font-semibold"}>
-                                Apply Now!
-                            </button>
+                            <div className={"flex flex-col w-11/12 items-center gap-2"}>
+                                <div className={"flex mt-2"}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#ef4444">
+                                        <path d="M480.06-486.67q30.27 0 51.77-21.56 21.5-21.55 21.5-51.83 0-30.27-21.56-51.77-21.55-21.5-51.83-21.5-30.27 0-51.77 21.56-21.5 21.55-21.5 51.83 0 30.27 21.56 51.77 21.55 21.5 51.83 21.5ZM480-168q129.33-118 191.33-214.17 62-96.16 62-169.83 0-114.86-73.36-188.1-73.36-73.23-179.97-73.23T300.03-740.1q-73.36 73.24-73.36 188.1 0 73.67 63 169.83Q352.67-286 480-168Zm0 88Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Zm0-480Z"/>
+                                    </svg>
+                                    <p className={"font-medium"}>{vacancy.location}</p>
+                                </div>
+                                <button
+                                    className={"p-1.5 w-full bg-red-100 border border-red-700 rounded-md hover:bg-red-700 hover:text-white transition-ease-in duration-300 text-red-900 font-semibold"}>
+                                    Apply Now!
+                                </button>
+                            </div>
                         </div>
                     </div>
                 ) : (
